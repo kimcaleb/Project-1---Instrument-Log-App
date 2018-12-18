172bf6181ba3bfa0c7bd59a81ec31f0c6cb058c8
Caleb Kim <calebkim2005@yahoo.com>
2018-12-18 15:36:16 -0800

    Established Device/log model endpoints

---
+++ routes/devices.js
@@ -0,0 +1,15 @@
+const   
+    express = require("express"),
+    Device = require("../controllers"),
+    Log = require("../controllers"),
+    deviceRouter = express.Router();
+
+
+
+// Device Endpoints
+deviceRouter.get("/", Device.index); // show all devices
+deviceRouter.post("/", Device.create); // create new device
+
+// Log Endpoints
+deviceRouter.get("/:id", Log.index); // Show a specific device + logs. 
+deviceRouter.post("/:id", Log.create); // Create new log
