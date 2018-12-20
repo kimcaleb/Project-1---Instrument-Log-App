196827d09ce99597016e2a32e58ce2e2b3064661
Caleb Kim <calebkim2005@yahoo.com>
2018-12-20 00:11:52 -0800

    Header

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.use((req,res,next) => {
 	app.locals.currentUser = req.user;
     app.locals.loggedIn = req.user;
     app.locals.newDevice = req.newDevice;
+    app.locals.isEmpty = function(obj) {
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
+    }
 	next();
 });
 
